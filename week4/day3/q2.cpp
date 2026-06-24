6ea93c971a4539b2dcf1469dd1dd2fbf7b21c2b4
shixamrawat <shixamrawat@gmail.com>
2026-06-24 16:48:56 +0530

    Day 3 Week 4

---
+++ week4/day3/q2.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool helper(TreeNode* root,TreeNode* min,TreeNode* max){
+        if(root==nullptr){
+            return true;
+        }
+        if(min!=nullptr && root->val<=min->val){
+            return false;
+        }
+        if(max!=nullptr && root->val>=max->val){
+            return false;
+        }
+
+        return helper(root->left,min,root) && 
+        helper(root->right,root,max);
+
+    }
+    bool isValidBST(TreeNode* root) {
+        return helper(root,nullptr,nullptr);
+    }
+};
